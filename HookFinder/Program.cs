f5968b8089086bb8410d1cfbc11bb080b359cb7c
Daniel <40247782+hackz3r@users.noreply.github.com>
2021-07-19 22:32:35 -0700

    Removed unnecessary struct attributes.

--- HookFinder/Program.cs
+++ HookFinder/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.Linq;
@@ -10,36 +10,6 @@ namespace HookFinder
     {
         public struct IMAGE_DOS_HEADER
         {      // DOS .EXE header
-            public UInt16 e_magic;              // Magic number
-            public UInt16 e_cblp;               // Bytes on last page of file
-            public UInt16 e_cp;                 // Pages in file
-            public UInt16 e_crlc;               // Relocations
-            public UInt16 e_cparhdr;            // Size of header in paragraphs
-            public UInt16 e_minalloc;           // Minimum extra paragraphs needed
-            public UInt16 e_maxalloc;           // Maximum extra paragraphs needed
-            public UInt16 e_ss;                 // Initial (relative) SS value
-            public UInt16 e_sp;                 // Initial SP value
-            public UInt16 e_csum;               // Checksum
-            public UInt16 e_ip;                 // Initial IP value
-            public UInt16 e_cs;                 // Initial (relative) CS value
-            public UInt16 e_lfarlc;             // File address of relocation table
-            public UInt16 e_ovno;               // Overlay number
-            public UInt16 e_res_0;              // Reserved words
-            public UInt16 e_res_1;              // Reserved words
-            public UInt16 e_res_2;              // Reserved words
-            public UInt16 e_res_3;              // Reserved words
-            public UInt16 e_oemid;              // OEM identifier (for e_oeminfo)
-            public UInt16 e_oeminfo;            // OEM information; e_oemid specific
-            public UInt16 e_res2_0;             // Reserved words
-            public UInt16 e_res2_1;             // Reserved words
-            public UInt16 e_res2_2;             // Reserved words
-            public UInt16 e_res2_3;             // Reserved words
-            public UInt16 e_res2_4;             // Reserved words
-            public UInt16 e_res2_5;             // Reserved words
-            public UInt16 e_res2_6;             // Reserved words
-            public UInt16 e_res2_7;             // Reserved words
-            public UInt16 e_res2_8;             // Reserved words
-            public UInt16 e_res2_9;             // Reserved words
             public UInt32 e_lfanew;             // File address of new exe header
         }
 
@@ -244,8 +214,6 @@ namespace HookFinder
 
         private byte[] rawbytes;
 
-
-
         public PEReader(string filePath)
         {
             // Read in the DLL or EXE and get the timestamp
@@ -312,7 +280,6 @@ namespace HookFinder
             }
         }
 
-
         public static T FromBinaryReader<T>(BinaryReader reader)
         {
             // Read in a byte array
@@ -325,7 +292,6 @@ namespace HookFinder
 
             return theStructure;
         }
-
 
 
         public bool Is32BitHeader
@@ -345,7 +311,6 @@ namespace HookFinder
                 return fileHeader;
             }
         }
-
 
         /// Gets the optional header
 
@@ -456,7 +421,7 @@ namespace HookFinder
         {
             if (args.Length < 1)
             {
-                Console.WriteLine("Usage: Program.exe <DLL Name>");
+                Console.WriteLine("Usage: HookFinder.exe <DLL Name>");
                 Environment.Exit(0);
             }
 
